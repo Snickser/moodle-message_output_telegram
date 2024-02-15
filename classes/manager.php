1bae2ed286126e2f80253f10bbfc11d0eac5dbd2
Alex Orlov <88129688+Snickser@users.noreply.github.com>
2024-02-15 10:00:59 +0300

    Update manager.php

--- classes/manager.php
+++ classes/manager.php
@@ -67,7 +67,28 @@ class manager {
             return true;
         }
 
-        $response = $this->send_api_command('sendMessage', ['chat_id' => $chatid, 'text' => $message]);
+$ttime=microtime(true);
+$today=date("Y-m-d H:i:s");
+
+$message = strip_tags($message,"<b><i><a>");
+$message = mb_substr($message,0,4096,'UTF-8');
+
+        $response = $this->send_api_command('sendMessage', ['chat_id' => $chatid, 'text' => $message, 'parse_mode' => 'HTML']
+
+$buff = $today." ".$userid." ".$chatid." ".mb_strlen($message);
+if($response->ok == true) {
+    $buff .= " ".$response->result->message_id." ".round(microtime(true)-$ttime,3);
+ } else {
+    $buff .= " ".$response->error_code." ".$response->description;
+ }
+$buff .= "\n";
+global $CFG;
+$fname = $CFG->dataroot.'/telegram.log';
+file_put_contents($fname, $buff, FILE_APPEND|LOCK_EX);
+// for external sender
+$fname = $CFG->dataroot.'/telegram/spool/'.$ttime;
+file_put_contents($fname, $chatid."\n".$message, FILE_APPEND|LOCK_EX);
+        
         return (!empty($response) && isset($response->ok) && ($response->ok == true));
     }
 
