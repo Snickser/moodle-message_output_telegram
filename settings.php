6ce8b623402da618c7420793ac559f19e2b25852
Alex <88129688+Snickser@users.noreply.github.com>
2024-02-25 08:42:14 +0300

    Update settings.php
    add logging

--- settings.php
+++ settings.php
@@ -74,9 +74,14 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configcheckbox('message_telegram/striptags',
         get_string('striptags', 'message_telegram'),
         get_string('configstriptags', 'message_telegram'), true));
+
     $options = array('' => get_string('parse_text', 'message_telegram'), 'HTML' => get_string('parse_html', 'message_telegram'));
     $settings->add(new admin_setting_configselect('message_telegram/parsemode', get_string('parsemode', 'message_telegram'),
         get_string('configparsemode', 'message_telegram'), '', $options));
+
+    $settings->add(new admin_setting_configcheckbox('message_telegram/telegramlog',
+        get_string('telegramlog', 'message_telegram'),
+        get_string('configtelegramlog', 'message_telegram'), true));
 
 //    $url = new moodle_url('/message/output/telegram/telegramconnect.php', ['sesskey' => sesskey(), 'action' => 'setwebhook']);
 //    $link = html_writer::link($url, get_string('setwebhook', 'message_telegram'));
