b8e249227284109266f099404a60c3a64ae2c69d
Snickser <snickser@gmail.com>
2024-08-01 16:26:30 +0300

    version

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 3024060700; // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 3024080100; 
 $plugin->requires = 2016111500; // Moodle version.
 
 $plugin->component = 'message_telegram';
 
-$plugin->release  = '3.3 (Build - 2024022500)';
+$plugin->release  = '3.3';
 $plugin->maturity  = MATURITY_STABLE;
