c027985836b648fdef64a275a3aa005aacbe5bb9
Snickser <snickser@gmail.com>
2025-08-25 21:02:59 +0300

    add

--- lang/en/message_telegram.php
+++ lang/en/message_telegram.php
@@ -25,6 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['alreadyconnected'] = '✅ Your Telegram account is linked!';
 $string['configsitebotname'] = 'This will be filled in automatically when you save the bot token.';
 $string['configsitebottoken'] = 'Enter the site bot token from Botfather here.';
 $string['configsitebotusername'] = 'This will be filled in automatically when you save the bot token.';
@@ -32,7 +33,8 @@ $string['connectinstructions'] = 'Once you have clicked the link below, you will
 your Telegram account. In Telegram, click the "Start" button in the "{$a}" chat that opens to connect your account to Moodle.
 Once completed, come back to this page and click "Save changes". Full documentation
 <a href="https://docs.moodle.org/33/en/Telegram_message_processor#Configuring_user_preferences" target="_blank">here</a>.';
-$string['connectme'] = 'Connect my account to Telegram.';
+$string['connectme'] = 'Connect my account to Telegram';
+$string['connectmemenu'] = 'Connect my account to Telegram';
 $string['notconfigured'] = 'The Telegram server hasn\'t been configured so Telegram messages cannot be sent';
 
 $string['configparsemode'] = 'Formatting options: Text or HTML.';
