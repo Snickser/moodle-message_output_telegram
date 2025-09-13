de7c22add752079cf67c03e53534a3668ed42c4d
Alex <snickser@gmail.com>
2025-09-13 11:24:13 +0300

    add

--- telegramconnect.php
+++ telegramconnect.php
@@ -32,22 +32,23 @@ $PAGE->set_url(new moodle_url('/message/output/telegram/telegramconnect.php'));
 $PAGE->set_context(context_system::instance());
 
 require_login();
+require_sesskey();
 
 $telegrammanager = new message_telegram\manager();
 
 if ($action == 'setwebhook') {
-    require_sesskey();
     require_capability('moodle/site:config', context_system::instance());
     if (strpos($CFG->wwwroot, 'https:') !== 0) {
         $message = get_string('requirehttps', 'message_telegram');
     } else {
         if (empty(get_config('message_telegram', 'webhook'))) {
             $message = $telegrammanager->set_webhook();
-        }
+        } else {
+	    $message = $telegrammanager->remove_webhook();
+	}
     }
     redirect(new moodle_url('/admin/settings.php', ['section' => 'messagesettingtelegram']), $message);
 } else if ($action == 'removechatid') {
-    require_sesskey();
     $userid = optional_param('userid', 0, PARAM_INT);
     if ($userid != 0) {
         $message = $telegrammanager->remove_chatid($userid);
