2de7cc43719ea9e5e55ed220899049d60ff31ecb
Alex <snickser@gmail.com>
2025-09-18 10:35:04 +0300

    add

--- lang/en/message_telegram.php
+++ lang/en/message_telegram.php
@@ -26,68 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['alreadyconnected'] = '✅ Your Telegram account is linked';
-$string['configsitebotname'] = 'This will be filled in automatically when you save the bot token.';
-$string['configsitebottoken'] = 'Enter the site bot token from Botfather here.';
-$string['configsitebotusername'] = 'This will be filled in automatically when you save the bot token.';
-$string['connectinstructions'] = 'Once you have clicked the link below, you will need to allow the link to open in Telegram with
-your Telegram account. In Telegram, click the "Start" button in the "{$a}" chat that opens to connect your account to Moodle.
-Once completed, come back to this page and click "Save changes". Full documentation
-<a href="https://docs.moodle.org/33/en/Telegram_message_processor#Configuring_user_preferences" target="_blank">here</a>.';
-$string['connectme'] = 'Connect my account to Telegram';
-$string['connectmemenu'] = 'Connect my account to Telegram';
-$string['customfield'] = 'If there is an additional user profile field "telegram_username", it will be filled in automatically.';
-
-$string['notconfigured'] = 'The Telegram server hasn\'t been configured so Telegram messages cannot be sent';
-
-$string['configparsemode'] = 'Formatting options: Text or HTML.';
-$string['parsemode'] = 'Parse mode';
-$string['parse_text'] = 'Text only';
-$string['parse_html'] = 'HTML format';
-$string['striptags'] = 'Strip tags';
-$string['configstriptags'] = 'Strip all html tags from "Text" formatted message (in "HTML" mode unresolved tags are always removed).';
-$string['telegramlog'] = 'Enable logging';
-$string['configtelegramlog'] = 'Write debug info into ' . $CFG->tempdir . '/telegram.log file.';
-$string['telegramlogdump'] = 'Dump message to log';
-$string['configtelegramlogdump'] = 'For debugging purposes, write the message to a log file.';
-$string['tgext'] = 'Path to external sender';
-$string['configtgext'] = 'You may need to use an external messaging service, such as bypass ratelimit, or ensure that messages are delivered.';
-
-$string['pluginname'] = 'Telegram';
-$string['sitebotname'] = 'Bot name for site';
-$string['sitebottoken'] = 'Bot token for site';
-$string['sitebottokennotsetup'] = 'Bot token for site must be specified in plugin settings.';
-$string['sitebotusername'] = 'Bot username for site';
-$string['telegrambottoken'] = 'Telegram bot token';
-$string['telegramchatid'] = 'Telegram chat id';
-$string['removetelegram'] = 'Remove Telegram connection';
-$string['requirehttps'] = 'Site must use HTTPS for Telegram\'s webhook function.';
-$string['setupinstructions'] = 'Create a new Telegram Bot using Botfather. Click the Botfather link below and open it in Telegram.
-Use the "/newbot" command in Telegram to start creating the bot. You will need to specify a botname, for example "{$a->name}", and a
-unique bot username, for example "{$a->username}". Full documentation
-<a href="https://docs.moodle.org/33/en/Telegram_message_processor" target="_blank">here</a>.';
-$string['setwebhook'] = 'Setup Telegram webhook';
-
-$string['fullmessagehtml'] = 'Use fullmessagehtml';
-$string['configfullmessagehtml'] = 'Get message from "$eventdata->fullmessagehtml" (if available), or from "fullmessage" if not set.';
-$string['donate'] = '<div>Plugin version: {$a->release} ({$a->versiondisk})</br>
-You can find new versions of the plugin at <a href=https://github.com/Snickser/moodle-message_output_telegram>GitHub.com</a>
-<img src="https://img.shields.io/github/v/release/Snickser/moodle-message_output_telegram.svg"><br>
-Please send me some <a href="https://yoomoney.ru/fundraise/143H2JO3LLE.240720">donate</a>😊</div>
-TON UQA1vhoJmBLgzTHKbJuuscr6UPwnP9TEH3eJYFKIiVgUIaro<br>
-BTC 1GFTTPCgRTC8yYL1gU7wBZRfhRNRBdLZsq<br>
-TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
-<iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=143H2JO3LLE.240720"
-width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no"></iframe>';
-$string['welcome'] = '✅ Your account has been successfully linked!';
-$string['telegramwebhook'] = 'Webhook';
-$string['configtelegramwebhook'] = 'This Telegram webhook is for testing purposes, do not enable it, otherwise push button twice!';
-$string['sitebotsecret'] = 'Webhook secret';
-$string['configsitebotsecret'] = 'Generated randomly and automatically if empty.';
-$string['firstregister'] = 'First, register on the site ';
-$string['sitebotpay'] = 'Payment token';
-$string['configsitebotpay'] = 'Bot payment token for accepting payments.';
-$string['sitebotpaycosts'] = 'Predefined amounts';
-$string['configsitebotpaycosts'] = 'Separated by commas.';
+$string['botenrols'] = '🎓 Participation in courses:';
+$string['botfaq'] = '⁉️ Frequently Asked Questions:';
+$string['botfaqtext'] = '';
 $string['bothelp'] = '👓 Helps
 /info - platform information
 /faq  - frequently asked questions
@@ -99,10 +40,69 @@ $string['bothelp_anonymous'] = '👓 Helps
 /info - platform information
 /faq  - frequently asked questions';
 $string['botidontknow'] = 'I dont know what this is 🤷🏻 /help';
-$string['botenrols'] = '🎓 Participation in courses:';
 $string['botlang'] = '🈯 Select language ({$a})';
 $string['botpay'] = '🏦 Select amount {$a}';
-$string['botpaytitle'] = 'Donation 🕉';
 $string['botpaydesc'] = 'To support the learning platform';
-$string['botfaq'] = '⁉️ Frequently Asked Questions:';
-$string['botfaqtext'] = '';
+$string['botpaytitle'] = 'Donation 🕉';
+$string['configfullmessagehtml'] = 'Get message from "$eventdata->fullmessagehtml" (if available), or from "fullmessage" if not set.';
+$string['configparsemode'] = 'Formatting options: Text or HTML.';
+$string['configsitebotname'] = 'This will be filled in automatically when you save the bot token.';
+$string['configsitebotpay'] = 'Bot payment token for accepting payments.';
+$string['configsitebotpaycosts'] = 'Separated by commas.';
+$string['configsitebotsecret'] = 'Generated randomly and automatically if empty.';
+$string['configsitebottoken'] = 'Enter the site bot token from Botfather here.';
+$string['configsitebotusername'] = 'This will be filled in automatically when you save the bot token.';
+$string['configstriptags'] = 'Strip all html tags from "Text" formatted message (in "HTML" mode unresolved tags are always removed).';
+$string['configtelegramlog'] = 'Write debug info into ' . $CFG->tempdir . '/telegram.log file.';
+$string['configtelegramlogdump'] = 'For debugging purposes, write the message to a log file.';
+$string['configtelegramwebhook'] = 'This Telegram webhook is for testing purposes, do not enable it, otherwise push button twice!';
+$string['configtgext'] = 'You may need to use an external messaging service, such as bypass ratelimit, or ensure that messages are delivered.';
+$string['connectinstructions'] = 'Once you have clicked the link below, you will need to allow the link to open in Telegram with
+your Telegram account. In Telegram, click the "Start" button in the "{$a}" chat that opens to connect your account to Moodle.
+Once completed, come back to this page and click "Save changes". Full documentation
+<a href="https://docs.moodle.org/33/en/Telegram_message_processor#Configuring_user_preferences" target="_blank">here</a>.';
+$string['connectme'] = 'Connect my account to Telegram';
+$string['connectmemenu'] = 'Connect my account to Telegram';
+$string['customfield'] = 'If there is an additional user profile field "telegram_username", it will be filled in automatically.';
+
+$string['donate'] = '<div>Plugin version: {$a->release} ({$a->versiondisk})</br>
+You can find new versions of the plugin at <a href=https://github.com/Snickser/moodle-message_output_telegram>GitHub.com</a>
+<img src="https://img.shields.io/github/v/release/Snickser/moodle-message_output_telegram.svg"><br>
+Please send me some <a href="https://yoomoney.ru/fundraise/143H2JO3LLE.240720">donate</a>😊</div>
+TON UQA1vhoJmBLgzTHKbJuuscr6UPwnP9TEH3eJYFKIiVgUIaro<br>
+BTC 1GFTTPCgRTC8yYL1gU7wBZRfhRNRBdLZsq<br>
+TRX TRGMc3b63Lus6ehLasbbHxsb2rHky5LbPe<br>
+<iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=143H2JO3LLE.240720"
+width="330" height="50" frameborder="0" allowtransparency="true" scrolling="no"></iframe>';
+$string['firstregister'] = 'First, register on the site ';
+$string['fullmessagehtml'] = 'Use fullmessagehtml';
+$string['notconfigured'] = 'The Telegram server hasn\'t been configured so Telegram messages cannot be sent';
+
+$string['parse_html'] = 'HTML format';
+$string['parse_text'] = 'Text only';
+$string['parsemode'] = 'Parse mode';
+$string['pluginname'] = 'Telegram';
+$string['removetelegram'] = 'Remove Telegram connection';
+$string['requirehttps'] = 'Site must use HTTPS for Telegram\'s webhook function.';
+$string['setupinstructions'] = 'Create a new Telegram Bot using Botfather. Click the Botfather link below and open it in Telegram.
+Use the "/newbot" command in Telegram to start creating the bot. You will need to specify a botname, for example "{$a->name}", and a
+unique bot username, for example "{$a->username}". Full documentation
+<a href="https://docs.moodle.org/33/en/Telegram_message_processor" target="_blank">here</a>.';
+$string['setwebhook'] = 'Setup Telegram webhook';
+$string['sitebotname'] = 'Bot name for site';
+$string['sitebotpay'] = 'Payment token';
+$string['sitebotpaycosts'] = 'Predefined amounts';
+$string['sitebotsecret'] = 'Webhook secret';
+$string['sitebottoken'] = 'Bot token for site';
+$string['sitebottokennotsetup'] = 'Bot token for site must be specified in plugin settings.';
+$string['sitebotusername'] = 'Bot username for site';
+$string['striptags'] = 'Strip tags';
+$string['telegrambottoken'] = 'Telegram bot token';
+$string['telegramchatid'] = 'Telegram chat id';
+$string['telegramlog'] = 'Enable logging';
+$string['telegramlogdump'] = 'Dump message to log';
+$string['telegramwebhook'] = 'Webhook';
+$string['tgext'] = 'Path to external sender';
+
+
+$string['welcome'] = '✅ Your account has been successfully linked!';
--- lang/ru/message_telegram.php
+++ lang/ru/message_telegram.php
@@ -26,24 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['alreadyconnected'] = '✅ Ваш Telegram аккаунт подключен';
-$string['configsitebotname'] = 'Будет заполнено автоматически, когда Вы сохраните токен бота.';
-$string['configsitebottoken'] = 'Введите сюда токен бота сайта, полученный от Botfather.';
-$string['configsitebotusername'] = 'Будет заполнено автоматически, когда Вы сохраните токен бота.';
-$string['notconfigured'] = 'Сервер Telegram не настроен, поэтому сообщения Telegram не могут быть отправлены';
-$string['pluginname'] = 'Telegram';
-$string['removetelegram'] = 'Отключиться от Telegram';
-$string['setupinstructions'] = 'Создайте новый бот Telegram, используя Botfather. Перейдите по ссылке Botfather ниже и откройте Telegram.
-Используйте команду "/newbot" в Telegram для начала создания бота. Вам надо будет задать название бота, например "{$a->name}" и уникальное имя бота, например "{$a->username}". Полное описание <a href="https://docs.moodle.org/33/en/Telegram_message_processor" target="_blank">читаем здесь</a>.';
-$string['sitebotname'] = 'Название бота для сайта';
-$string['sitebottoken'] = 'Токен бота для сайта';
-$string['sitebottokennotsetup'] = 'Токен бота для сайта должен быть указан в настройках плагина.';
-$string['sitebotusername'] = 'Ник бота для сайта';
-$string['telegrambottoken'] = 'Токен бота Telegram';
-$string['telegramchatid'] = 'ID чата Telegram';
-$string['connectinstructions'] = 'После того, как вы нажмёте ссылку ниже, вам нужно будет разрешить открытие ссылки в Telegram с вашей учётной записью Telegram. В Telegram нажмите кнопку «Start» в открывшемся чате «{$a}», чтобы подключить свою учётную запись.<br>После завершения вернитесь на эту страницу и нажмите «Save changes (Сохранить изменения)». Полную инструкцию <a href="https://docs.moodle.org/33/en/Telegram_message_processor#Configuring_user_preferences" target="_blank">читаем здесь</a>.';
-$string['connectme'] = '<br><p style="color: blue;"><b>> Подключить свой аккаунт к Telegram <</b></p>';
-$string['connectmemenu'] = '⚠️ Подключить свой аккаунт к Telegram';
-$string['welcome'] = '✅ Ваш аккаунт успешно подключен!';
+$string['botenrols'] = '🎓 Участие в курсах:';
+$string['botfaq'] = '⁉️ Часто задаваемые вопросы:';
+$string['botfaqtext'] = '';
 $string['bothelp'] = '👓 Подсказки
 /info - информация о платформе
 /faq  - часто задаваемые вопросы
@@ -55,10 +40,26 @@ $string['bothelp_anonymous'] = '👓 Подсказки
 /info - информация о платформе
 /faq  - часто задаваемые вопросы';
 $string['botidontknow'] = 'Не знаю что это такое 🤷🏻 /help';
-$string['botenrols'] = '🎓 Участие в курсах:';
 $string['botlang'] = '🈯 Выбрать язык ({$a})';
 $string['botpay'] = '🏦 Выберите сумму {$a}';
-$string['botpaytitle'] = 'Пожертвование 🕉';
 $string['botpaydesc'] = 'На поддержание учебной платформы';
-$string['botfaq'] = '⁉️ Часто задаваемые вопросы:';
-$string['botfaqtext'] = '';
+$string['botpaytitle'] = '🕉 Пожертвование 🕉';
+$string['configsitebotname'] = 'Будет заполнено автоматически, когда Вы сохраните токен бота.';
+$string['configsitebottoken'] = 'Введите сюда токен бота сайта, полученный от Botfather.';
+$string['configsitebotusername'] = 'Будет заполнено автоматически, когда Вы сохраните токен бота.';
+$string['connectinstructions'] = 'После того, как вы нажмёте ссылку ниже, вам нужно будет разрешить открытие ссылки в Telegram с вашей учётной записью Telegram. В Telegram нажмите кнопку «Start» в открывшемся чате «{$a}», чтобы подключить свою учётную запись.<br>После завершения вернитесь на эту страницу и нажмите «Save changes (Сохранить изменения)». Полную инструкцию <a href="https://docs.moodle.org/33/en/Telegram_message_processor#Configuring_user_preferences" target="_blank">читаем здесь</a>.';
+$string['connectme'] = '<br><p style="color: blue;"><b>> Подключить свой аккаунт к Telegram <</b></p>';
+$string['connectmemenu'] = '⚠️ Подключить свой аккаунт к Telegram';
+$string['firstregister'] = 'Сначала зарегистрируйтесь на сайте, и подключите уведомления через Телеграм. {$a}';
+$string['notconfigured'] = 'Сервер Telegram не настроен, поэтому сообщения Telegram не могут быть отправлены';
+$string['pluginname'] = 'Telegram';
+$string['removetelegram'] = 'Отключиться от Telegram';
+$string['setupinstructions'] = 'Создайте новый бот Telegram, используя Botfather. Перейдите по ссылке Botfather ниже и откройте Telegram.
+Используйте команду "/newbot" в Telegram для начала создания бота. Вам надо будет задать название бота, например "{$a->name}" и уникальное имя бота, например "{$a->username}". Полное описание <a href="https://docs.moodle.org/33/en/Telegram_message_processor" target="_blank">читаем здесь</a>.';
+$string['sitebotname'] = 'Название бота для сайта';
+$string['sitebottoken'] = 'Токен бота для сайта';
+$string['sitebottokennotsetup'] = 'Токен бота для сайта должен быть указан в настройках плагина.';
+$string['sitebotusername'] = 'Ник бота для сайта';
+$string['telegrambottoken'] = 'Токен бота Telegram';
+$string['telegramchatid'] = 'ID чата Telegram';
+$string['welcome'] = '✅ Ваш аккаунт успешно подключен!';
