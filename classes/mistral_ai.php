bec70e880ede8df0a3d577be31438ff4d4c94d78
Alex <snickser@gmail.com>
2026-02-27 14:29:14 +0300

    fix

--- classes/mistral_ai.php
+++ classes/mistral_ai.php
@@ -311,6 +311,8 @@ class mistral_ai {
         } catch (\Exception $e) {
             debugging('Failed to get Mistral models: ' . $e->getMessage(), DEBUG_DEVELOPER);
         }
+
+        return [];
     }
 
     /**
