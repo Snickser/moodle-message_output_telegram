c6e165423423641a5f05c82b0e25b4250414946d
Snickser <snickser@gmail.com>
2026-05-27 06:59:59 +0300

    tune

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 3026042324;
+$plugin->version   = 3026052724;
 $plugin->requires  = 2016111500;
 $plugin->component = 'message_telegram';
 $plugin->release   = '4.0.18';
